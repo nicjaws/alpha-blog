0875cd5b83b72af322ed6edb10a8dc063a4676f9
Nicolas Corbalan <outreef@hotmail.com>
2018-12-13 09:56:20 +0000

    Add edit update functionality and partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,48 +13,34 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
    # @user = User.first
-
     if @user.save
-
       flash[:success] = "Bienvenido a Aduanas #{@user.username}"
-
       redirect_to articles_path
-
     else
-
       render 'new'
-
     end
-
   end
   
   def edit
-
     @user = User.find(params[:id])
-
   end
+
 
   def update
-
     @user = User.find(params[:id])
-
     if @user.update(user_params)
-
-    flash[:success] = "Your account was updated successfully"
-
+    flash[:success] = "Tu cuenta fue actualizada con exito"
     redirect_to articles_path
-
     else
-
     render 'edit'
-
     end
-
   end
+  
   
   def show 
     
   end 
+  
 
 private
 
@@ -63,9 +49,7 @@ private
   end
 
   def user_params
-
-  params.require(:user).permit(:username, :email, :password)
-
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
