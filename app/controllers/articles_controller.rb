dc9b6ed3d592bcc5df8480937373f18b57a9784e
Nicolas Corbalan <outreef@hotmail.com>
2018-12-18 13:30:32 +0000

    adding security to users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def index 
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -14,8 +16,8 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    @article = Article.new(article_params)
-    @article.user = User.first
+    
+    @article = current_user.articles.create(article_params)
     if @article.save
       flash[:success] = "Articulo creado exitosamente"
       redirect_to article_path(@article)
@@ -57,4 +59,12 @@ class ArticlesController < ApplicationController
     def article_params
     params.require(:article).permit(:title, :description)
     end
+    
+    def require_same_user
+      if current_user != @article.user
+        flash[:danger] = "Tu solo puedes editar o borrar tus propios articulos"
+        redirect_to root_path
+      end
+    end
+    
 end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   
-  before_action :set_user, only: [:show]
-  
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update] 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -22,12 +22,11 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    
   end
 
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
     flash[:success] = "Tu cuenta fue actualizada con exito"
     redirect_to articles_path
@@ -38,23 +37,27 @@ class UsersController < ApplicationController
   
   
   def show 
-    @user = User.find(params[:id])
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end 
   
 
 private
 
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+
   def set_user
     @user = User.find (params[:id])
   end
-
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
+  
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "Solo puedes editar tu propia cuenta" 
+      redirect_to root_path
+    end
   end
 
+ 
 end
-
-private
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
-  end
